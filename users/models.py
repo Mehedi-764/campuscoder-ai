141f172c801abbd8befac69731dc137941d0d769
Mehedi-764 <2464cse01158@manarat.ac.bd>
2026-05-19 12:03:26 +0600

    dashboard_update

--- users/models.py
+++ users/models.py
@@ -12,8 +12,28 @@ class AIQuery(models.Model):
 
     response = models.TextField()
 
+    is_favorite = models.BooleanField(default=False)
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
 
         return self.user.username
+
+
+class Activity(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    action = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+
+        return self.action

25a729967c61864ffe68764357e7b7096382bd56
Mehedi Hasan Sabbir <2464cse01158@manarat.ac.bd>
2026-08-13 09:47:31 +0600

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import history, register, login_view, dashboard, logout_view, home, assistant, favorite_chat, visualizer
+from .views import history, register, login_view, dashboard, logout_view, home, assistant, favorite_chat, visualizer, roadmap, module, verify_otp
 
 urlpatterns = [
+
+    path('roadmap/', roadmap, name='roadmap'),
+
+    path('module/', module, name='module'),
 
     path('visualizer/', visualizer, name='visualizer'),
 
@@ -12,6 +16,8 @@ urlpatterns = [
     path('', home, name='home'),
 
     path('register/', register, name='register'),
+
+    path("verify-otp/", verify_otp, name="verify_otp"),
 
     path('login/', login_view, name='login'),
 
